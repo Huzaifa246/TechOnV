741eb635bc035712c8d6b2c25fd169c4ce60f055
Huzaifa246 <huzaifa24680@gmail.com>
2023-04-08 02:56:30 -0700

    Graph and swiper changes

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -16,10 +16,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/effect-cards";
 import { EffectCards } from "swiper";
-
-import {
-    LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer
-} from "recharts";
+import LineChartGraph from './LineChartGraph';
 
 const Item = styled(Paper)(({ theme }) => ({
     backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -29,21 +26,6 @@ const Item = styled(Paper)(({ theme }) => ({
     color: theme.palette.text.secondary,
 }));
 export default function Dashboard() {
-
-    const data = [
-        { name: "Jan", uv: 4000, pv: 2400 },
-        { name: "Feb", uv: 3000, pv: 1398 },
-        { name: "Mar", uv: 2000, pv: 9800 },
-        { name: "Apr", uv: 2780, pv: 3908 },
-        { name: "May", uv: 1890, pv: 4800 },
-        { name: "Jun", uv: 2390, pv: 3800 },
-        { name: "Jul", uv: 3490, pv: 4300 },
-        { name: "Aug", uv: 4000, pv: 2400 },
-        { name: "Sep", uv: 3000, pv: 1398 },
-        { name: "Oct", uv: 2000, pv: 9800 },
-        { name: "Nov", uv: 2780, pv: 3908 },
-        { name: "Dec", uv: 1890, pv: 4800 }
-    ];
     return (
         <>
             <Box sx={{ flexGrow: 1 }}>
@@ -227,25 +209,15 @@ export default function Dashboard() {
                     </Item>
                 </Grid>
                 <Grid item xs={12} md={8}>
-                    <Item>
+                    <Item >
                         <Typography sx={{ fontSize: 24 }} color="text.secondary" gutterBottom>
                             Transaction Trending
                         </Typography>
                         <hr />
-                        <ResponsiveContainer width="100%" height={300}>
-                            <LineChart width={1100} height={300} data={data}>
-                                <XAxis dataKey="name" />
-                                <YAxis />
-                                <CartesianGrid strokeDasharray="3 3" />
-                                <Tooltip />
-                                <Legend />
-                                <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
-                                <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
-                            </LineChart>
-                        </ResponsiveContainer >
+                        <LineChartGraph />
                     </Item>
                 </Grid>
-            </Grid>
+            </Grid >
 
 
         </>
--- src/Components/LineChartGraph.js
+++ src/Components/LineChartGraph.js
@@ -1,77 +1,36 @@
 import React from 'react';
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
-
-const data = [
-    {
-        name: 'Page A',
-        uv: 4000,
-        pv: 2400,
-        amt: 2400,
-    },
-    {
-        name: 'Page B',
-        uv: 3000,
-        pv: 1398,
-        amt: 2210,
-    },
-    {
-        name: 'Page C',
-        uv: 2000,
-        pv: 9800,
-        amt: 2290,
-    },
-    {
-        name: 'Page D',
-        uv: 2780,
-        pv: 3908,
-        amt: 2000,
-    },
-    {
-        name: 'Page E',
-        uv: 1890,
-        pv: 4800,
-        amt: 2181,
-    },
-    {
-        name: 'Page F',
-        uv: 2390,
-        pv: 3800,
-        amt: 2500,
-    },
-    {
-        name: 'Page G',
-        uv: 3490,
-        pv: 4300,
-        amt: 2100,
-    },
-];
-const demoUrl = 'https://codesandbox.io/s/line-chart-double-y-axes-4j73x';
+import {
+    LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer
+} from "recharts";
 
 function LineChartGraph() {
+    const data = [
+        { name: "Jan", uv: 4000, pv: 2400 },
+        { name: "Feb", uv: 3000, pv: 1398 },
+        { name: "Mar", uv: 2000, pv: 9800 },
+        { name: "Apr", uv: 2780, pv: 3908 },
+        { name: "May", uv: 1890, pv: 4800 },
+        { name: "Jun", uv: 2390, pv: 3800 },
+        { name: "Jul", uv: 3490, pv: 4300 },
+        { name: "Aug", uv: 4000, pv: 2400 },
+        { name: "Sep", uv: 3000, pv: 1398 },
+        { name: "Oct", uv: 2000, pv: 9800 },
+        { name: "Nov", uv: 2780, pv: 3908 },
+        { name: "Dec", uv: 1890, pv: 4800 }
+    ];
     return (
         <>
-            <ResponsiveContainer width="100%" height="100%">
-                <LineChart
-                    width={500}
-                    height={300}
-                    data={data}
-                    margin={{
-                        top: 5,
-                        right: 30,
-                        left: 20,
-                        bottom: 5,
-                    }}
-                >
-                    <CartesianGrid strokeDasharray="3 3" />
+            <ResponsiveContainer width="100%" height={300}>
+                <LineChart width={1100} height={300} data={data}>
                     <XAxis dataKey="name" />
-                    <YAxis yAxisId="left" />
-                    <YAxis yAxisId="right" orientation="right" />
+                    <YAxis />
+                    <CartesianGrid strokeDasharray="3 3" />
                     <Tooltip />
                     <Legend />
-                    <Line yAxisId="left" type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
-                    <Line yAxisId="right" type="monotone" dataKey="uv" stroke="#82ca9d" />
+                    <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
+                    <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
                 </LineChart>
-            </ResponsiveContainer>
+            </ResponsiveContainer >
         </>
     )
 }
